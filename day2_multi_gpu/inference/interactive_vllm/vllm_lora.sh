86a8759d456837c9480c040b32735ea869f7760e
Hicham Agueny <h.agueny@gmail.com>
2026-03-18 17:10:02 +0100

    add interactive example

---
+++ day2_multi_gpu/inference/interactive_vllm/vllm_lora.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Exit on error
+set -e
+module load NRIS/GPU
+module load CUDA/12.9.1
+module load vLLM/0.11.0
+
+# -------- User configuration --------
+PROJECT_DIR="/cluster/work/projects/nn9997k"
+MyWD="$PROJECT_DIR/$USER/llm-hpc-course"
+CURRENT_DIR="${MyWD}/day2_multi_gpu/inference/task_QA"
+
+# Python path for inference
+PYTHON_FILE="${MyWD}/recipes/inference/vllm_inference.py"
+
+# Set paths
+MODEL_PATH=${MODEL_PATH:-"${MyWD}/shared/models/Llama-3.1-8B-Instruct"}
+LORA_PATH=${LORA_PATH:-"${MyWD}/results/checkpoints_out/llama3_1_8B_lora_multi_device/epoch_0"}
+PROMPT_FILE=${PROMPT_FILE:-"$CURRENT_DIR/prompt_QA.json"}
+QUANTIZATION=${QUANTIZATION:-"None"}  # Set to "bitsandbytes" for QLoRA
+
+echo "----------------------------------------"
+echo "Configuration:"
+echo "  Model: $MODEL_PATH"
+echo "  LoRA: $LORA_PATH"
+echo "  Prompt file: $PROMPT_FILE"
+echo "  Quantization: $QUANTIZATION"
+echo "----------------------------------------"
+
+# Verify prompt file exists
+if [[ ! -f "$PROMPT_FILE" ]]; then
+    echo "ERROR: Prompt file not found: $PROMPT_FILE"
+    exit 1
+fi
+
+export VLLM_CACHE_ROOT=$MyWD/.cache/vllm
+if [ ! -d "$VLLM_CACHE_ROOT" ]; then
+  mkdir -p "$VLLM_CACHE_ROOT"
+fi
+
+# Suppress Logs (Set to ERROR to hide Warnings/Info)
+export VLLM_LOGGING_LEVEL=ERROR
+
+# Monitoring logs
+LOG_DIR="${MyWD}/day2_multi_gpu/inference/logs"
+if [ ! -d "$LOG_DIR" ]; then
+   mkdir -p ${MyWD}/day2_multi_gpu/inference/logs	
+fi
+# --- Start GPU Monitoring in the background ---
+export MONITOR_LOG="$LOG_DIR/gpu_lora_utilization_${SLURM_JOB_ID}.csv"
+python $MyWD/utils/gpu_monitor.py --interval 3 --output "$MONITOR_LOG" &
+MONITOR_PID=$!
+echo "Started GPU monitor (PID: $MONITOR_PID) logging to $MONITOR_LOG"
+
+# -------- Launch --------
+python3 "$PYTHON_FILE" \
+          --model "$MODEL_PATH" \
+          --lora-path "$LORA_PATH" \
+          --prompt-file "$PROMPT_FILE" \
+          --quantization "$QUANTIZATION"
+	
+echo "Job finished at: $(date)"
+
+# --- Stop GPU Monitoring ---
+echo "Stopping GPU monitor..."
+kill $MONITOR_PID
