5f57ec988a8b01f182f0fb40521848497588eba2
Hicham Agueny <h.agueny@gmail.com>
2026-03-18 14:40:24 +0100

    delete tempo files

--- day2_multi_gpu/inference/task_QA/slurm_vllm_lora.sh
+++ day2_multi_gpu/inference/task_QA/slurm_vllm_lora.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -A nn9970k
+#SBATCH -A nn9997k
 #SBATCH -p accel
 #SBATCH -t 00:10:00
 #SBATCH -N 1
@@ -16,7 +16,7 @@ module load NRIS/GPU
 module load vLLM/0.11.0
 
 # -------- User configuration --------
-PROJECT_DIR="/cluster/work/projects/nn9970k"
+PROJECT_DIR="/cluster/work/projects/nn9997k"
 MyWD="$PROJECT_DIR/$USER/llm-hpc-course"
 CURRENT_DIR="${MyWD}/day2_multi_gpu/inference/task_QA"
 
