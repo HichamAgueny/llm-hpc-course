5cbdc145957e2bb443544c154104bbe999ee6d97
Hicham Agueny <h.agueny@gmail.com>
2026-06-03 17:00:22 +0200

    update scripts

--- day2_multi_gpu/inference/task_QA/slurm_vllm_distributed.sh
+++ day2_multi_gpu/inference/task_QA/slurm_vllm_distributed.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH -A nn9970k
+#SBATCH -A nn9997k
 #SBATCH -p accel
-#SBATCH -t 00:15:00
+#SBATCH -t 00:10:00
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=4
 #SBATCH --gpus=4             
-#SBATCH --mem-per-cpu=8G
+#SBATCH --mem-per-gpu=97G
 #SBATCH -J vllm-distributed
 #SBATCH -o ./out/%x-%j.out
 #SBATCH -e ./out/%x-%j.err
@@ -13,19 +13,20 @@
 # Exit on error
 set -e
 module load NRIS/GPU
-module load CUDA/12.9.1
 module load vLLM/0.11.0
 
+export PYTHONNOUSERSITE=1
+
 # -------- User configuration --------
-PROJECT_DIR="/cluster/work/projects/nn9970k"
+PROJECT_DIR="/cluster/projects/nn9997k"
 MyWD="$PROJECT_DIR/$USER/llm-hpc-course"
 CURRENT_DIR="${MyWD}/day2_multi_gpu/inference/task_QA"
 # Python path for inference
 PYTHON_FILE="${MyWD}/recipes/inference/vllm_distributed.py"
 
 # Set paths
-MODEL_PATH=${MODEL_PATH:-"${MyWD}/shared/models/Llama-3.1-8B-Instruct"}
-LORA_PATH=${LORA_PATH:-"${MyWD}/results/checkpoints_out/llama3_1_8B_lora_multi_device/epoch_0"}
+MODEL_PATH=${MODEL_PATH:-"${MyWD}/shared/models/Llama-3.2-1B-Instruct"}
+LORA_PATH=${LORA_PATH:-"${MyWD}/results/checkpoints_out/llama3_2_1B_lora_multi_device/epoch_0"}
 PROMPT_FILE=${PROMPT_FILE:-"$CURRENT_DIR/prompt_QA.json"}
 QUANTIZATION=${QUANTIZATION:-"None"}  # Set to "bitsandbytes" for QLoRA
 
@@ -62,7 +63,7 @@ if [ ! -d "$LOG_DIR" ]; then
 fi
 # --- Start GPU Monitoring in the background ---
 export MONITOR_LOG="$LOG_DIR/multi_gpu_utilization_${SLURM_JOB_ID}.csv"
-python $MyWD/utils/gpu_monitor.py --interval 3 --output "$MONITOR_LOG" &
+python $MyWD/utils/monitoring/gpu_monitor.py --interval 3 --output "$MONITOR_LOG" &
 MONITOR_PID=$!
 echo "Started GPU monitor (PID: $MONITOR_PID) logging to $MONITOR_LOG"
 
@@ -72,38 +73,20 @@ export MASTER_PORT=25900
 export WORLD_SIZE=$SLURM_NPROCS
 export LOCAL_WORLD_SIZE=$SLURM_GPUS_PER_NODE
 
-# --- Create the Inner Script (runs INSIDE container) ---
-INNER_SCRIPT_TEMP="./.my_script_temp_${SLURM_JOB_ID}"
-cat > "${INNER_SCRIPT_TEMP}" << EOF
-#!/bin/bash -e
-
 # Set up variables to control distributed PyTorch training
-export RANK=\$SLURM_PROCID
-export LOCAL_RANK=\$SLURM_LOCALID
+export RANK=$SLURM_PROCID
+export LOCAL_RANK=$SLURM_LOCALID
 
-echo "Task \${SLURM_PROCID}: RANK=${SLURM_PROCID}, LOCAL_RANK=${SLURM_LOCALID}, WORLD_SIZE = $WORLD_SIZE, LOCAL_WORLD_SIZE = $LOCAL_WORLD_SIZE"
-echo "LOCAL_RANK: \${LOCAL_RANK}, CUDA_VISIBLE_DEVICES: \${CUDA_VISIBLE_DEVICES}"
+echo "Task ${SLURM_PROCID}: RANK=${SLURM_PROCID}, LOCAL_RANK=${SLURM_LOCALID}, WORLD_SIZE = $WORLD_SIZE, LOCAL_WORLD_SIZE = $LOCAL_WORLD_SIZE"
+echo "LOCAL_RANK: ${LOCAL_RANK}, CUDA_VISIBLE_DEVICES: ${CUDA_VISIBLE_DEVICES}"
 
 # Run the inference script
-python "${PYTHON_FILE}" \
+srun python "${PYTHON_FILE}" \
     --model "$MODEL_PATH" \
     --lora-path "$LORA_PATH" \
     --prompt-file "$PROMPT_FILE"
     --tensor-parallel-size "$TP_SIZE" \
     --pipeline-parallel-size "$PP_SIZE"  
-#
-# Syntax of "tune run" command
-#the flag --standalone is Useful when launching single-node, multi-worker job
-#If --standalone specified then the options --rdzv-backend, --rdzv-endpoint, --rdzv-id are auto-assigned and any explicitly set values are ignored.
-
-#tune run --nnodes $N --nproc_per_node $nproc_perN --standalone lora_finetune_distributed --config "${CONFIG_FILE}" checkpointer.output_dir="${OUTPUT_DIR}" output_dir="${LOGGING_DIR}" epochs=1
-EOF
-
-chmod +x "${INNER_SCRIPT_TEMP}"
-
-# --- Suppress LMOD Debugging ---
-export LMOD_SH_DBG_ON=0
-srun "${INNER_SCRIPT_TEMP}"
 
 echo "Job finished at: $(date)"
 
