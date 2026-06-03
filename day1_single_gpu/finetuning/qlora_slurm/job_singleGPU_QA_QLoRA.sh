9a0cf1191dc630105200c42613f2d453d9c3780c
Hicham Agueny <h.agueny@gmail.com>
2026-06-03 16:50:29 +0200

    update jobs

--- day1_single_gpu/finetuning/qlora_slurm/job_singleGPU_QA_QLoRA.sh
+++ day1_single_gpu/finetuning/qlora_slurm/job_singleGPU_QA_QLoRA.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-#SBATCH --job-name=ft-llama3-1B-qlora-1gpu_xsum
+#SBATCH --job-name=ft-llama3-1B-qlora-1gpu
 #SBATCH --account=nn9997k
 #SBATCH --time=00:15:00
 #SBATCH --partition=accel
@@ -21,7 +21,7 @@ CONTAINER_DIR="${MyWD}/apptainer"
 APPTAINER_SIF="${CONTAINER_DIR}/pytorch_25.08_cuda13.0_arm_custom.sif"
 
 # Configs and python files for fine-tuning
-CONFIG_FILE="${MyWD}/configs/qlora/llama3_2_1B_qlora_single_device_XSum.yaml"
+CONFIG_FILE="${MyWD}/configs/qlora/llama3_2_1B_qlora_single_device_QA.yaml"
 PYTHON_FILE="${MyWD}/recipes/single_device/lora_finetune_single_device.py"
 
 echo "--- My Main Directory (host): ${MyWD}"
@@ -32,6 +32,39 @@ echo "CONFIG_FILE: ${CONFIG_FILE}"
 echo "PYTHON_FILE: ${PYTHON_FILE}"
 echo
 
+# --- Create the Inner Script (runs INSIDE container) ---
+INNER_SCRIPT_TEMP="./.my_script_temp_${SLURM_JOB_ID}"
+
+cat > "${INNER_SCRIPT_TEMP}" << EOF
+#!/bin/bash -e
+
+# Flash Attention for efficiency
+export USE_FLASH_ATTENTION=1
+
+# Verify critical files exist
+if [ ! -f "${PYTHON_FILE}" ]; then
+  echo "-- ERROR: Python script not found at ${PYTHON_FILE}"
+  exit 1
+fi
+
+if [ ! -f "${CONFIG_FILE}" ]; then
+  echo "--ERROR: Config file not found at ${CONFIG_FILE}"
+  exit 1
+fi
+
+# Run the fine-tuning script
+python "${PYTHON_FILE}" --config "${CONFIG_FILE}"
+
+EOF
+
+chmod +x "${INNER_SCRIPT_TEMP}"
+
+# --- Suppress LMOD Debugging ---
+export LMOD_SH_DBG_ON=0
+# --- Locale Settings ---
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+
 echo "--- Launching the application inside Apptainer ---"
 
 # --- Execute with Apptainer ---
@@ -41,7 +74,10 @@ time srun apptainer exec --nv \
       -B "${MyWD}:/workspace" \
        -B $PROJECT_DIR \
       "${APPTAINER_SIF}" \
-       python "${PYTHON_FILE}" --config "${CONFIG_FILE}"
+      "${INNER_SCRIPT_TEMP}"
+
+# --- Clean Up Temporary Script ---
+rm -f "${INNER_SCRIPT_TEMP}"
 
 echo
 echo "--- Finished :) ---"
